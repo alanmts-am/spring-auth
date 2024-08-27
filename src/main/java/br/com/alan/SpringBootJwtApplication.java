ae69d4aebc4ad791cef33ef22b025d4dab722894
Alan Mateus <76778046+alanmts-am@users.noreply.github.com>
2024-08-26 23:08:09 -0300

    refactor: add CORS

--- src/main/java/br/com/alan/SpringBootJwtApplication.java
+++ src/main/java/br/com/alan/SpringBootJwtApplication.java
@@ -2,9 +2,21 @@ package br.com.alan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
-public class SpringBootJwtApplication {
+public class SpringBootJwtApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtApplication.class, args);
